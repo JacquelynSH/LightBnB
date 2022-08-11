19795c00378b17f67b6d7787f5dd6dee5ec9035c
JacquelynSH <elpatchaembroidery@gmail.com>
2022-08-10 19:31:42 -0700

    Refactor addProperty function to add a new property

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -152,15 +152,13 @@ exports.getAllReservations = getAllReservations;
   `;
 
   // 5
-  console.log(queryString, queryParams);
+  // console.log(queryString, queryParams);
 
   // 6
   return pool.query(queryString, queryParams)
   .then((res) => res.rows)
   .catch((err) => console.log(err.message));
 };
-
-// DO I NEED AN ELSE? 
 
 exports.getAllProperties = getAllProperties;
 
@@ -171,9 +169,23 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const newProperty = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url,
+  property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, 
+  property.city, property.province, property.post_code];
+  console.log(newProperty);
+  const queryString = `INSERT INTO properties (owner_id, title, 
+    description, thumbnail_photo_url, cover_photo_url, cost_per_night,
+     parking_spaces, number_of_bathrooms, number_of_bedrooms, country, 
+     street, city, province, post_code)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;`;
+    return pool.query(queryString, newProperty)
+   .then((result) => {
+     return result.rows;
+   })
+   .catch((err) => {
+     console.error(err.message);
+   });
 }
+
 exports.addProperty = addProperty;
